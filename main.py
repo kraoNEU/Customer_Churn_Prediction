0ee9df9f3d7e3b86d8f2aaa40a87032360ac5286
kraoNEU <chelamkuri.v@northeastern.edu>
2022-08-18 12:28:56 -0400

    pushed my chages

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import pandas as pd
+
 from src import dataset_Reader
 from src import feature_Selection
 
 dataFrame_Churn_Prediction = dataset_Reader.dataset_Reader()
 
-X = dataFrame_Churn_Prediction.drop('churn', axis=1)
-y = dataFrame_Churn_Prediction["churn"]
-print(feature_Selection.feature_Selection(X, y))
+# Getting the features_X of the Dataframe
+features_X = dataFrame_Churn_Prediction.drop('churn', axis=1)
+
+# features_X dataframe has got 'string' values which cannot be processed my feature selectors, convert them to identifiers
+features_X = pd.get_dummies(features_X)
+
+# target_Label for the Target Label
+target_y = dataFrame_Churn_Prediction["churn"]
+
+# target_Label dataframe has got 'string' values which cannot be processed my feature selectors, convert them to identifiers
+target_y = pd.get_dummies(target_y)
+
+print(feature_Selection.feature_Selection(features_X, target_y))
--- src/dataset_Reader.py
+++ src/dataset_Reader.py
@@ -6,6 +6,6 @@ def dataset_Reader():
     :return: The Dataset in the csv format
     """
 
-    df = pd.read_csv("https://www.neuraldesigner.com/files/datasets/bank_churn.csv", sep=";", index_col=None)
+    df = pd.read_csv("https://www.nteuraldesigner.com/files/datasets/bank_churn.csv", sep=";", index_col=None)
     df.to_csv("dataset/Bank_Churn_Prediction.csv")
     return df
